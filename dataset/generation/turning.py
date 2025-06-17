44adb3b9b8ce7196403842794ff84fd1cc4a4654
KunChan Kim <mplngx2@gmail.com>
2025-06-17 11:48:29 +0900

    터닝 관련 코드 구조 이동

--- dataset/generation/turning.py
+++ dataset/generation/turning.py
@@ -1,121 +1,47 @@
-from typing import List
+import random
+from typing import List, Tuple
 
 from OCC.Core.gp import gp_Ax2, gp_Pnt, gp_Dir
-from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeCylinder, BRepPrimAPI_MakeCone
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeCylinder, BRepPrimAPI_MakeCone, BRepPrimAPI_MakeBox
+from OCC.Core.BRepFilletAPI import BRepFilletAPI_MakeChamfer
 from OCC.Core.BRepFilletAPI import BRepFilletAPI_MakeFillet
-from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Fuse   
+from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Fuse, BRepAlgoAPI_Cut
+from OCC.Core.BRepAdaptor import BRepAdaptor_Curve
 from OCC.Core.TopoDS import TopoDS_Shape, TopoDS_Edge
 from OCC.Core.TopAbs import TopAbs_EDGE, TopAbs_FACE, TopAbs_VERTEX
 from OCC.Core.TopExp import TopExp_Explorer
-from OCC.Core.BRepAdaptor import BRepAdaptor_Curve
 from OCC.Core.GeomAbs import GeomAbs_Circle
-from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
-from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Cut
 
 
-class TurningMachiningModelGenerator:
-    def __init__(self, 
-                 stock_height: float=10.0, 
-                 stock_radius: float=2.0):
-        self.origin: gp_Pnt = gp_Pnt(0, 0, 0)
-        self.direction: gp_Dir = gp_Dir(0, 0, 1)
-        self.height: float = stock_height
-        self.radius: float = stock_radius
-        self.stock_model: TopoDS_Shape = self._create_stock_model()
-        self.is_top_turning: bool = False
-        self.is_bottom_turning: bool = False
-
-
-    def _create_stock_model(self) -> TopoDS_Shape:
-        axis = gp_Ax2(self.origin, self.direction)  
-        cylinder = BRepPrimAPI_MakeCylinder(axis, self.radius, self.height).Shape()
-        return cylinder
-
-    def _search__edge_on(self, opt: str ='top') -> List[TopoDS_Edge]:
-        def is_on_top_circle_edge(edge: TopoDS_Edge) -> bool:
-            edge_curve = BRepAdaptor_Curve(edge)
-            if edge_curve.GetType() == GeomAbs_Circle:
-                circle = edge_curve.Circle()
-                circle_center = circle.Location()
-                circle_height = circle_center.Z()
-                return abs(circle_height - self.height) <= 1e-6
-            return False
-        
-        def is_on_bottom_circle_edge(edge: TopoDS_Edge) -> bool:
-            edge_curve = BRepAdaptor_Curve(edge)
-            if edge_curve.GetType() == GeomAbs_Circle:
-                circle = edge_curve.Circle()
-                circle_center = circle.Location()
-                circle_height = circle_center.Z()
-                return abs(circle_height) <= 1e-6
-            return False    
-        
-        edge_list: List[TopoDS_Edge] = []
-        edge_exp = TopExp_Explorer(self.stock_model, TopAbs_EDGE)   
-        while edge_exp.More():
-            edge = edge_exp.Current()
-            if edge.IsNull():    
-                edge_exp.Next() 
-                continue
-            try:
-                if opt == 'top' and is_on_top_circle_edge(edge):
-                    edge_list.append(edge)  
-                elif opt == 'bottom' and is_on_bottom_circle_edge(edge):
-                    edge_list.append(edge)
-            except Exception as e:
-                raise ValueError(f"Error processing edge: {e}")
-            edge_exp.Next()
-        return edge_list
-
-    def set_stock_properties(self, height: float, radius: float) -> None:
-        self.height = height
-        self.radius = radius
-        self.stock_model = self._create_stock_model()
-        return
+class TurningMainShapeGenerator:
+    def __init__(self, radius: float, height: float):
+        self.radius: float = radius
+        self.height: float = height
+        self.stock_shape: TopoDS_Shape = self._generate_cylinder_shape(radius=radius, height=height)
     
-    def set_straight_on(self, opt: str = 'top', height1: float=1.0, height2: float=2.0) -> None:
-        return NotImplementedError("Straight functionality is not yet implemented.")
     
-    def set_rough_on(self, opt: str = 'top', height: float=5.0) -> None:
-        return NotImplementedError("Rough functionality is not yet implemented.")
-    
-    def set_turing_chamfer_on(self, opt: str = 'top', height: float=5.0) -> None:
-        return NotImplementedError("Turning chamfer functionality is not yet implemented.")
-    
-    def set_tapper_on(self, opt: str='top', height: float=1.0) -> None:  
+    def tapper(self, opt: str='top', height: float=1.0) -> None:  
         if opt == 'top':    
-            center = gp_Pnt(
-                self.origin.X() + self.direction.X() * (self.height - height),
-                self.origin.Y() + self.direction.Y() * (self.height - height), 
-                self.origin.Z() + self.direction.Z() * (self.height - height))
-            direction = gp_Dir(
-                self.direction.X(),
-                self.direction.Y(),
-                self.direction.Z()) 
+            center = gp_Pnt(0.0, 0.0, self.height - height)
+            direction = gp_Dir(0.0, 0.0, 1.0) 
             
             box = BRepPrimAPI_MakeBox(
                 gp_Pnt(-self.radius, -self.radius, self.height - height),
                 2*self.radius,
                 2*self.radius,
                 height).Shape() 
-            self.stock_model = BRepAlgoAPI_Cut(self.stock_model, box).Shape()   
+            self.stock_shape = BRepAlgoAPI_Cut(self.stock_shape, box).Shape()   
             self.is_top_turning = True
         elif opt == 'bottom':
-            center = gp_Pnt(
-                self.origin.X() + self.direction.X() * height,
-                self.origin.Y() + self.direction.Y() * height,
-                self.origin.Z() + self.direction.Z() * height)
-            direction = gp_Dir( 
-                -self.direction.X(),
-                -self.direction.Y(),
-                -self.direction.Z())
+            center = gp_Pnt(0.0, 0.0, height)
+            direction = gp_Dir(0.0, 0.0, -1.0)
             
-            box = BRepPrimAPI_MakeBox(
+            box = BRepPrimAPI_MakeBox(  
                 gp_Pnt(-self.radius, -self.radius, 0),
                 2*self.radius,
                 2*self.radius,
                 height).Shape() 
-            self.stock_model = BRepAlgoAPI_Cut(self.stock_model, box).Shape()
+            self.stock_shape = BRepAlgoAPI_Cut(self.stock_shape, box).Shape()
             self.is_bottom_turning = True
         else:
             raise ValueError("Invalid option for cone placement. Use 'top' or 'bottom'.")   
@@ -123,87 +49,169 @@ class TurningMachiningModelGenerator:
         
         cone_axis = gp_Ax2(center, direction)
         cone = BRepPrimAPI_MakeCone(cone_axis, self.radius, 0, height).Shape()
-        fused_shape = BRepAlgoAPI_Fuse(self.stock_model, cone).Shape()  
-        self.stock_model = fused_shape  
-        return
+        fused_shape = BRepAlgoAPI_Fuse(self.stock_shape, cone).Shape()  
+        self.stock_shape = fused_shape  
+        return self.stock_shape 
     
-    def set_turing_fillet_on(self, opt: str='top', radius: float=0.1) -> None:  
-        fillet = BRepFilletAPI_MakeFillet(self.stock_model)
-        if opt == 'top':
-            top_edges: List[TopoDS_Edge] = self._search__edge_on(opt='top')
-            for edge in top_edges:
-                fillet.Add(radius, edge)
-            self.is_top_turning = True
-        elif opt == 'bottom':   
-            bottom_edges: List[TopoDS_Edge] = self._search__edge_on(opt='bottom')
-            for edge in bottom_edges:
-                fillet.Add(radius, edge)
-            self.is_bottom_turning = True       
-        self.stock_model = fillet.Shape()
-        return
-    
-    def set_grooving_S(self, 
-                       height: float=5.0, 
-                       depth: float=0.5, 
-                       width: float=1.0) -> None:        
-        groove_center = gp_Pnt(
-            self.origin.X(),
-            self.origin.Y(), 
-            self.origin.Z() + height)
+    def grooving(
+        self,
+        height: float = 1.0,
+        depth: float = 1.0,
+        width: float = 1.0) -> TopoDS_Shape:
+        if height + width > self.height:
+            raise ValueError("Height and width exceed the stock shape height.") 
+        if depth >= self.radius:
+            raise ValueError("Depth must be less than the radius of the stock shape.")  
         
-        groove_outer_radius = self.radius + 1.0 
-        groove_axis = gp_Ax2(groove_center, self.direction)
-        groove_outer = BRepPrimAPI_MakeCylinder(
-            groove_axis, groove_outer_radius, width).Shape()
+        groove_shape: TopoDS_Shape = self._generate_tube_shape(
+            outer_radius=self.radius, 
+            inner_radius=self.radius - depth, 
+            height=height, 
+            width=width) 
+        self.stock_shape = self._extrude_cut(
+            stock_shape=self.stock_shape,   
+            extrusion_shape=groove_shape)   
+        return self.stock_shape
+    
+    def step(
+        self,
+        height: float = 1.0,
+        depth: float = 1.0,
+        width: float =1.0) -> TopoDS_Shape:
+        if height + width > self.height:
+            raise ValueError("Height and width exceed the stock shape height.") 
+        step_shape: TopoDS_Shape = self._generate_tube_shape(
+            outer_radius=self.radius + depth, 
+            inner_radius=self.radius, 
+            height=height, 
+            width=width) 
+        self.stock_shape = self._fuse_shapes(
+            stock_shape=self.stock_shape,   
+            fuse_shape=step_shape)   
+        return self.stock_shape
+    
+    def _generate_tube_shape(self, 
+                             outer_radius: float, 
+                             inner_radius: float, 
+                             height: float,
+                             width: float) -> TopoDS_Shape:
+        axis = gp_Ax2(gp_Pnt(0, 0, height), gp_Dir(0, 0, 1))
+        outer_cylinder = BRepPrimAPI_MakeCylinder(axis, outer_radius, width).Shape()
+        inner_cylinder = BRepPrimAPI_MakeCylinder(axis, inner_radius, width).Shape()
+        return self._extrude_cut(stock_shape=outer_cylinder, extrusion_shape=inner_cylinder)
+    
+    def _generate_cylinder_shape(self, radius: float, height: float) -> TopoDS_Shape:   
+        axis = gp_Ax2(gp_Pnt(0, 0, 0), gp_Dir(0, 0, 1))
+        return BRepPrimAPI_MakeCylinder(axis, radius, height).Shape()   
+    
+    def _extrude_cut(self, 
+                     stock_shape: TopoDS_Shape, 
+                     extrusion_shape: TopoDS_Shape):
+        cut_operation = BRepAlgoAPI_Cut(stock_shape, extrusion_shape)
+        cut_operation.Build()
         
-        groove_inner_radius = self.radius - depth
-        groove_inner = BRepPrimAPI_MakeCylinder(
-            groove_axis, groove_inner_radius, width).Shape()
+        if cut_operation.IsDone():
+            return cut_operation.Shape()
+        else:
+            raise RuntimeError("Failed to perform extrude cut operation")   
+    
+    def _fuse_shapes(self, 
+                     stock_shape: TopoDS_Shape, 
+                     fuse_shape: TopoDS_Shape) -> TopoDS_Shape:
+        fuse_operation = BRepAlgoAPI_Fuse(stock_shape, fuse_shape)
+        fuse_operation.Build()
         
-        groove_volume = BRepAlgoAPI_Cut(groove_outer, groove_inner).Shape()
-        result = BRepAlgoAPI_Cut(self.stock_model, groove_volume).Shape()
-        self.stock_model = result
-        return
-    
-    def set_grooving_T(self, 
-                    height: float=5.0,
-                    depth: float=0.5,
-                    width: float=1.0) -> None:
-        return NotImplementedError("Grooving T functionality is not yet implemented.")
-    
-    def set_step_S_on(self,
-                   height: float=5.0,
-                   width: float=1.0) -> None:
-        return NotImplementedError("Step functionality is not yet implemented.")
-    
-    def set_step_T_on(self,
-                     height: float=5.0,
-                     width: float=1.0) -> None:
-        return NotImplementedError("Step functionality is not yet implemented.")  
-    
-    def get_turning_machining_model(self) -> TopoDS_Shape:
-        return self.stock_model
+        if fuse_operation.IsDone():
+            return fuse_operation.Shape()
+        else:
+            raise RuntimeError("Failed to perform fuse operation")  
 
+class ChamferingRoundingGenerator:
+    def __init__(self,
+                 stock_shape: TopoDS_Shape):
+        self.stock_shape: TopoDS_Shape = stock_shape
+        self.circular_edges: List[Tuple[TopoDS_Edge, float, float]] = self._find_circular_edges()
+    
+    
+    def _find_circular_edges(self) -> List[Tuple[TopoDS_Edge, float, float]]:
+        circular_edges = []
+        edge_explorer = TopExp_Explorer(self.stock_shape, TopAbs_EDGE)
+        
+        while edge_explorer.More():
+            edge = edge_explorer.Current()
+            edge_explorer.Next()
+            
+            curve_adaptor = BRepAdaptor_Curve(edge)
+            
+            if curve_adaptor.GetType() == GeomAbs_Circle:
+                circle = curve_adaptor.Circle()
+                center = circle.Location()
+                radius = circle.Radius()
+                circular_edges.append((edge, center.Z(), radius))
+        
+        circular_edges.sort(key=lambda x: (x[1], x[2]))
+        print (f' {len(circular_edges)}')
+        return circular_edges
+    
+    def _apply_operation_by_index(self, radius: float, operation_type: str, index: int) -> TopoDS_Shape:
+        if index < 0 or index >= len(self.circular_edges):
+            raise ValueError(f"Invalid index {index}. Available indices: 0 to {len(self.circular_edges)-1}")
+        
+        target_edge = self.circular_edges[index][0]
+        
+        try:
+            if operation_type == "chamfer":
+                maker = BRepFilletAPI_MakeChamfer(self.stock_shape)
+                maker.Add(radius, target_edge)
+            elif operation_type == "round":
+                maker = BRepFilletAPI_MakeFillet(self.stock_shape)
+                maker.Add(radius, target_edge)
+            else:
+                raise ValueError("Invalid operation type")
+            
+            maker.Build()
+            
+            if maker.IsDone():
+                self.stock_shape = maker.Shape()
+                self.circular_edges = self._find_circular_edges()
+                print(f"{operation_type.capitalize()} applied to edge at index {index}")
+                return self.stock_shape
+            else:
+                print(f"Warning: Edge at index {index} is not suitable for {operation_type}")
+                return self.stock_shape
+                
+        except:
+            print(f"Warning: Edge at index {index} is not suitable for {operation_type}")
+            return self.stock_shape
+        
+    def chamfer(self, radius: float, index: int) -> TopoDS_Shape:
+        return self._apply_operation_by_index(radius, "chamfer", index)
+        
+    def round(self, radius: float, index: int) -> TopoDS_Shape:
+        return self._apply_operation_by_index(radius, "round", index)
 
 if __name__ == "__main__":
     from OCC.Display.SimpleGui import init_display
-    from OCC.Core.gp import gp_Pnt, gp_Dir
     
-    stock_height = 15.0
-    stock_radius = 5.0
+    origin = gp_Pnt(0, 0, 0)    
+    direction = gp_Dir(0, 0, 1) 
+    height = 15.0
+    radius = 4.0
     
-    turning_model_generator = TurningMachiningModelGenerator(
-        stock_height=stock_height,
-        stock_radius=stock_radius)
-    turning_model_generator.set_tapper_on('top', height=2.0)
-    turning_model_generator.set_turing_fillet_on('bottom', radius=0.5)
-    turning_model_generator.set_grooving_S(height=2.0, depth=0.5, width=2.0)
-    turning_model_generator.set_grooving_S(height=10.0, depth=1.5, width=1.0)
+    turning_shape_generator = TurningMainShapeGenerator(
+        radius=radius,
+        height=height)
+    turning_shape_generator.tapper('top', height=2.0)
+    turning_shape_generator.grooving(3.0, 0.5, 2.0)
+    turning_shape_generator.step(9.0, 1.0, 3.0)
+    ret = turning_shape_generator.step(8.0, 2.0, 3.0)
 
-    
-    stp_model = turning_model_generator.get_turning_machining_model()     
+    chamfer_round_generator = ChamferingRoundingGenerator(ret)
+    chamfer_round_generator.chamfer(0.5, 28)
+    chamfer_round_generator.round(0.4, 14)
+    ret= chamfer_round_generator.stock_shape
 
     display, start_display, add_menu, add_function_to_menu = init_display()
-    display.DisplayShape(stp_model, update=True)
-    
+    display.DisplayShape(ret, update=True)
+    display.FitAll()
     start_display()
